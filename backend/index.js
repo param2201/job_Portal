1d01dc3c10563baf351dff1fc120fe573c5a4ec0
Param Mistry <parammistryo3@gmail.com>
2024-08-07 17:58:02 +0530

    Created user schema

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
 dotenv.config({})
 
 const app = express(); 
@@ -19,5 +20,6 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
+    connectDB();
     console.log(`Server runnig at port ${PORT}`);
 })
