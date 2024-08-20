4d853cdf519c0b408c0cf420122e3822d3383c6a
Param Mistry <parammistryo3@gmail.com>
2024-08-20 17:47:42 +0530

    checked ipo connection

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,9 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-dotenv.config({})
+import userRoute from "./routes/user.route.js";
+
+dotenv.config({});
 
 const app = express(); 
 
@@ -15,9 +17,13 @@ const corsOptions = {
     origin: 'http//localhost:5173',
     credentials:true
 }
+
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
+
+//api's
+app.use("/api/v1/user",userRoute); 
 
 app.listen(PORT,()=>{
     connectDB();
