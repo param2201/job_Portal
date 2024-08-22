2576170bb85ee2e18a76e0d77b7ec8eb730c6184
Param Mistry <parammistryo3@gmail.com>
2024-08-22 19:52:09 +0530

    Company controller

--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 
 const companySchema = new mongoose.Schema({
     name:{
+        type:string,
+        required:true,
+        unique:true
     },
     desription:{
         type:String
